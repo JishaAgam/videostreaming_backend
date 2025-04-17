2170a3bbcb86fc329cc87e41c0ee5840c2ce3c3e
CB-agam <shipi.balamurugan@agamtech.com>
2025-04-17 13:07:20 +0530

    route check

--- run.py
+++ run.py
@@ -3,5 +3,9 @@ from app import create_app
 # Create app instance
 app = create_app()
 
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
